00ab579d99d716cd431d74b93a9073acfe6b8540
gerrygeop <gerryja28@gmail.com>
2021-05-03 02:04:08 +0800

    Update: Perhitungan

--- app/views/perhitungan/index.php
+++ app/views/perhitungan/index.php
@@ -1,18 +1,70 @@
 <div class="container">
     <div class="px-2 py-3 mb-5">
-        <h3 class="text-secondary">
+        <h3 class="text-secondary mb-4">
             Hasil Rekomendasi
         </h3>
 
         <!-- Start perhitungan -->
-        <div class="d-flex flex-column-reverse">
+        <div class="d-flex flex-column">
+
+            <div class="mb-5">
+
+                <div class="mb-4">
+                    <h4 class="text-secondary mb-0">#Pengujian Tingkat Akurasi</h4>
+                    <div class="row justify-content-center px-2">
+                        <div class="col py-2 px-4 my-3 shadow border border-1 rounded table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>TOPSIS</th>
+                                        <th>WP</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>94%</td>
+                                        <td>90%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div>
+                    <h4 class="text-secondary mb-0">#Uji Sensitivitas</h4>
+                    <div class="row justify-content-center px-2">
+                        <div class="col py-2 px-4 my-3 shadow border border-1 rounded table-responsive">
+                            <table class="table">
+                                <caption>
+                                    Berdasarkan pengujian menggunakan <b>Tingkat Akurasi</b> dan <b>Uji Sensitivitas</b> maka metode terbaik adalah metode FMADM TOPSIS karena memiliki nilai pengujian yang lebih besar
+                                </caption>
+                                <thead>
+                                    <tr>
+                                        <th>TOPSIS</th>
+                                        <th>WP</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>4,0537%</td>
+                                        <td>3,9049%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
 
             <div class="mb-5">
                 <h4 class="text-secondary">Ranking metode TOPSIS </h4>
 
                 <div class="row px-2">
 
-                    <div class="col-sm col-lg-6 py-2 px-4 my-3 shadow border rounded">
+                    <div class="col-sm col-lg-8 py-2 px-4 my-3 shadow border rounded">
                         <table class="table">
                             <thead>
                                 <tr>
@@ -56,73 +108,6 @@
                                         </td>
                                     </tr>
                                 <?php } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    
-                    <!-- Metode WP -->
-                    <div class="col-4 visually-hidden">
-                        <ul>
-                            <?php foreach($data['wp']['rankWp'] as $value) { ?>
-                                <li>
-                                    <?php $wp[] = $value; ?>
-                                </li>
-                            <?php } ?>
-                        </ul>
-                    </div>
-
-                </div>
-            </div>
-
-            <div class="mb-5">
-                <h4 class="text-secondary">Tingkat kesesuaisan nilai standard error</h4>
-                <div class="row justify-content-center px-2">
-
-                    <div class="col py-2 px-4 my-3 shadow border border-1 rounded table-responsive">
-                        <?php
-                            // TOPSIS
-                            $Tps = array_sum($tp);
-                            for ($i=0; $i <= $jumlahAlt-1; $i++) { 
-                                $Tpp[$i] = pow($tp[$i], 2);
-                            }
-                            $Tpp['aksen'] = array_sum($Tpp);
-                            
-                            $S = pow($Tps, 2) / $jumlahAlt;
-                            $S = ($Tpp['aksen'] - $S) / ($jumlahAlt-1);
-                            $S = sqrt($S);
-                            $SE = pow($S, 2) / $jumlahAlt;
-                            $SE = sqrt($SE);
-                            $TP = 100 - ($SE / 100);
-
-                            // WP
-                            $Wps = array_sum($wp);
-                            for ($i=0; $i <= $jumlahAlt-1; $i++) { 
-                                $Wpp[$i] = pow($wp[$i], 2); 
-                            }
-                            $Wpp['aksen'] = array_sum($Wpp);
-
-                            $S = pow($Wps, 2) / $jumlahAlt;
-                            $S = ($Wpp['aksen'] - $S) / ($jumlahAlt-1);
-                            $S = sqrt($S);
-                            $SE = pow($S, 2) / $jumlahAlt;
-                            $SE = sqrt($SE);
-                            $WP = 100 - ($SE / 100);
-                        ?>
-                        <table class="table">
-                            <caption>
-                                Berdasarkan tingkat kesesuaian standard error maka metode terbaik adalah metode TOPSIS karena memiliki nilai terkecil yaitu seebesar <?= substr($TP, 0, 8) . '%'; ?>
-                            </caption>
-                            <thead>
-                                <tr>
-                                    <th>TOPSIS</th>
-                                    <th>WP</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><?= substr($TP, 0, 8) . '%'; ?></td>
-                                    <td><?= substr($WP, 0, 8) . '%'; ?></td>
-                                </tr>
                             </tbody>
                         </table>
                     </div>
